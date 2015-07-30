8bba63c4d0f26147d4f8847698c181ac11060093
kyhfan <yh.kim@minivertising.kr>
2015-07-30 15:16:46 +0900

    111

--- include/dbi.php
+++ include/dbi.php
@@ -9,8 +9,8 @@
      *
      ******************************************************************************/
 	//$my_db = new mysqli("10.3.0.31", "root", "m!nv#Rtisin9", "babience_relay");
-	$my_db = new mysql("localhost", "root", "6alslqjxkdlwld@%*)", "vonin_aio");
-	if (mysql_connect_error()) {
-		exit('Connect Error (' . mysql_connect_errno() . ') '. mysql_connect_error());
+	$my_db = new mysqli("localhost", "root", "6alslqjxkdlwld@%*)", "vonin_aio");
+	if (mysqli_connect_error()) {
+		exit('Connect Error (' . mysqli_connect_errno() . ') '. mysqli_connect_error());
 	}
 ?>
