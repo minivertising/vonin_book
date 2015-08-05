7bdeb0f72f71ad7ddeb7f6a452182ac7ae92483f
kyhfan <yh.kim@minivertising.kr>
2015-08-05 18:44:18 +0900

    유튜브 영상 수정

--- include/global.php
+++ include/global.php
@@ -4,14 +4,14 @@
 	$_gl['comment_info_table']		= "comment_info";		//덧글 정보 테이블
 	$_gl['member_info_table']		= "member_info";			//참여자 정보 테이블
 
-	$_gl['m_yt_url1']	= "https://www.youtube.com/embed/3rBxaOJPVGk?controls=0&loop=1&modestbranding=1&showinfo=0&wmode=opaque&enablejsapi=0&rel=0"; // 유튜브 URL
-	$_gl['m_yt_url2']	= "https://www.youtube.com/embed/WWAwu4iYLE8?controls=0&loop=1&modestbranding=1&showinfo=0&wmode=opaque&enablejsapi=0&rel=0"; // 유튜브 URL
-	$_gl['m_yt_url3']	= "https://www.youtube.com/embed/h16U39DzIBc?controls=0&loop=1&modestbranding=1&showinfo=0&wmode=opaque&enablejsapi=0&rel=0"; // 유튜브 URL
-	$_gl['m_yt_url4']	= "https://www.youtube.com/embed/eefSEWT_1jI?controls=0&loop=1&modestbranding=1&showinfo=0&wmode=opaque&enablejsapi=0&rel=0"; // 유튜브 URL
+	$_gl['m_yt_url1']	= "https://www.youtube.com/embed/uqULeqzTI2w?controls=0&loop=1&modestbranding=1&showinfo=0&wmode=opaque&enablejsapi=0&rel=0"; // 유튜브 URL
+	$_gl['m_yt_url2']	= "https://www.youtube.com/embed/EDKRv_2K2cc?controls=0&loop=1&modestbranding=1&showinfo=0&wmode=opaque&enablejsapi=0&rel=0"; // 유튜브 URL
+	$_gl['m_yt_url3']	= "https://www.youtube.com/embed/7WbzHaeXoSs?controls=0&loop=1&modestbranding=1&showinfo=0&wmode=opaque&enablejsapi=0&rel=0"; // 유튜브 URL
+	$_gl['m_yt_url4']	= "https://www.youtube.com/embed/4eBLryCRBTk?controls=0&loop=1&modestbranding=1&showinfo=0&wmode=opaque&enablejsapi=0&rel=0"; // 유튜브 URL
 
-	$_gl['w_yt_url1']	= "https://www.youtube.com/embed/3rBxaOJPVGk?controls=0&loop=1&modestbranding=1&showinfo=0&wmode=opaque&enablejsapi=0&rel=0"; // 유튜브 URL
-	$_gl['w_yt_url2']	= "https://www.youtube.com/embed/WWAwu4iYLE8?controls=0&loop=1&modestbranding=1&showinfo=0&wmode=opaque&enablejsapi=0&rel=0"; // 유튜브 URL
-	$_gl['w_yt_url3']	= "https://www.youtube.com/embed/h16U39DzIBc?controls=0&loop=1&modestbranding=1&showinfo=0&wmode=opaque&enablejsapi=0&rel=0"; // 유튜브 URL
-	$_gl['w_yt_url4']	= "https://www.youtube.com/embed/eefSEWT_1jI?controls=0&loop=1&modestbranding=1&showinfo=0&wmode=opaque&enablejsapi=0&rel=0"; // 유튜브 URL
+	$_gl['w_yt_url1']	= "https://www.youtube.com/embed/1rU95PG4QA8?controls=0&loop=1&modestbranding=1&showinfo=0&wmode=opaque&enablejsapi=0&rel=0"; // 유튜브 URL
+	$_gl['w_yt_url2']	= "https://www.youtube.com/embed/FB_o6r_pO4A?controls=0&loop=1&modestbranding=1&showinfo=0&wmode=opaque&enablejsapi=0&rel=0"; // 유튜브 URL
+	$_gl['w_yt_url3']	= "https://www.youtube.com/embed/gxVuph4wbb0?controls=0&loop=1&modestbranding=1&showinfo=0&wmode=opaque&enablejsapi=0&rel=0"; // 유튜브 URL
+	$_gl['w_yt_url4']	= "https://www.youtube.com/embed/xk913pR3qpk?controls=0&loop=1&modestbranding=1&showinfo=0&wmode=opaque&enablejsapi=0&rel=0"; // 유튜브 URL
 
 ?>
