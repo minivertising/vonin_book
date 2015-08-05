c03594d620b87c2ccd649250df1af2dbc1408a13
kyhfan <yh.kim@minivertising.kr>
2015-08-05 22:21:20 +0900

    111

--- PC/comment_page.php
+++ PC/comment_page.php
@@ -504,4 +504,15 @@ function only_num(obj)
 	return true;
 }
 
+$(window).resize(resize);
+function resize(){
+	var __width  = $("body").width();
+	var __height = $("body").height();
+	var __scale  = Math.max(__width/1400, __height/850);
+	var __left   = Math.floor((__width-(1400*__scale))/2);
+	var __top    = Math.floor((__height-(850*__scale))/2);
+
+	$(".ar-full-bg").css({width:1400*__scale, height:850*__scale, left:__left, top:__top});
+}
+
 </script>
